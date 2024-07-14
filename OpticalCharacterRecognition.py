b19392066c1085f7a0d23e1cec2df3d3d676628e
NidhiPal14 <119780680+NidhiPal14@users.noreply.github.com>
2024-07-14 22:08:27 +0530

    Updated OpticalCharacterRecognition.py

--- OpticalCharacterRecognition.py
+++ OpticalCharacterRecognition.py
@@ -1,3 +1,4 @@
+#necessary libraries
 import pytesseract
 import cv2
 from googletrans import Translator, LANGUAGES
@@ -7,10 +8,10 @@ from tkinter import filedialog
 from pdf2image import convert_from_path
 import numpy as np
 
-
+#set tesseract command path
 pytesseract.pytesseract.tesseract_cmd = "D:\\Tesseract_f\\tesseract.exe"
 
-
+#function to open dialog box for prompting  user to upload image 
 def select_file():
     root = tk.Tk()
     root.withdraw()
@@ -29,7 +30,7 @@ root.geometry("400x200")
 
 file_path = None
 
-
+#handling pdf file or image selection 
 def on_select_file():
     global file_path
     file_path = select_file()
@@ -60,13 +61,17 @@ else:
         print(f"Error: Unable to read the image at {file_path}.")
         exit()
     images = [image]
-
+#applying binary thesholding to images using OTSU'S METHOD for preprocessing images
 for image in images:
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     processed_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
+
+    #text extraction using tesseract
     extracted_text = pytesseract.image_to_string(processed_image, lang='deu')
 
+    
+    #translation of extracted text
     translator = Translator()
     detected_language = translator.detect(extracted_text).lang
     print("Detected Language:", LANGUAGES.get(detected_language, "Unknown"))
@@ -76,22 +81,27 @@ for image in images:
     print(f"Extracted Text: {extracted_text}")
     print(f"Translated Text: {translated_text}")
 
+
+    #speaking aloud extracted text 
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
     engine.say(translated_text)
     engine.runAndWait()
 
+    #drawing bounding boxes and putting detected text over bounding boxes for clear veiw of extracted text in image 
     if not is_pdf:
         results = pytesseract.image_to_data(processed_image)
+        #print(results)  run this to see details of data extracted by tesseract from image 
         for ind, line in enumerate(results.splitlines()):
             if ind != 0:
                 line = line.split()
+                 #print(line) run this to see individual componenets of each line in image details
                 if len(line) == 12:
                     x, y, w, h = int(line[6]), int(line[7]), int(line[8]), int(line[9])
                     cv2.rectangle(image, (x, y), (w + x, h + y), (0, 255, 0), 2)
                     cv2.putText(image, line[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 1)
-
+        #display the image with bounding boxes
         cv2.imshow("Input", image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
